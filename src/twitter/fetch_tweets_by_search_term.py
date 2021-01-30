5d93cb0d6a562be82c6cf0aa66cc6b03f20e3043
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-01-29 22:06:10 -0400

    fix para _id

--- src/twitter/fetch_tweets_by_search_term.py
+++ src/twitter/fetch_tweets_by_search_term.py
@@ -9,6 +9,7 @@ import tweepy
 import os
 import sys
 import json
+from bson import ObjectId
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
 
@@ -92,7 +93,7 @@ def main():
         for tweet in tweets:
             tweet_json = tweet._json
             tweet_json['created_at'] = str(tweet.created_at)
-            tweet_json['_id'] = tweet.id
+            tweet_json['_id'] = ObjectId(tweet.id)
             tweet_json = json.dumps(tweet_json)
             try:
                 db.tweets.insert(tweet_json)
