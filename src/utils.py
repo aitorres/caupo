d3b302e99089e12b7d5c02e93f8381eae44a9a75
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-02-01 23:51:10 -0400

    feat: add context handler to track time of functions

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,30 @@
+import time
+import logging
+
 import pymongo
+
+logger = logging.getLogger("caupo")
 
 client = pymongo.MongoClient()
 db = client.caupo
+
+class Timer:
+    """Context handler to measure time of a function"""
+
+    def __init__(self, action_name):
+        logger.info("Starting: %s", action_name)
+        self.start_time = time.time()
+        self.end_time = None
+
+    def __enter__(self):
+        return self.start_time
+
+    def __exit__(self, type, value, traceback):
+        self.end_time = time.time()
+        self.duration = self.end_time - self.start_time
+
+        logger.info("Finishing: %s", action_name)
+        logger.debug("Done in %s milliseconds.", self.duration)
 
 
 def get_all_tweets():
