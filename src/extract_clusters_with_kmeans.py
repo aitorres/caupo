6f1089cb2192ded103a455940dd60269cd0dcb1b
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-02-02 00:02:03 -0400

    refactor: instantiate logger

--- src/extract_clusters_with_kmeans.py
+++ src/extract_clusters_with_kmeans.py
@@ -17,6 +17,16 @@ stop_words = set(stopwords.words('spanish'))
 
 # Instantiate logger
 logger = logging.getLogger("caupo")
+logger.setLevel(logging.DEBUG)
+
+# Create formatter
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+
+# Add console (standard output) handler to the logger
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # Get all tweets
 with Timer("Getting tweets' text from database"):
