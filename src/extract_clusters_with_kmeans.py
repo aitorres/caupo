dea64e7bbf75a2a4fcf5e5001a6eb18b2d5709ed
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-02-02 00:05:39 -0400

    fix: remove laziness through filters and maps

--- src/extract_clusters_with_kmeans.py
+++ src/extract_clusters_with_kmeans.py
@@ -44,7 +44,8 @@ with Timer("Normalizing tweets' text"):
     alphanumeric_corpus = map(partial(filter, lambda x: x.isalpha()), splitted_corpus)
 
     logger.info("Removing stopwords")
-    final_corpus = list(map(partial(filter, lambda x: x in stop_words), alphanumeric_corpus))
+    clean_corpus = list(map(partial(filter, lambda x: x in stop_words), alphanumeric_corpus))
+    final_corpus = list(map(list, clean_corpus))
 
 logger.info("Clean tweet example: %s", final_corpus[0])
 
