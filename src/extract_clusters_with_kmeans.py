f0ccaebbd6d290cd57223214136dac40539af401
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-02-02 00:08:11 -0400

    fix: boolean condition was reversed than expected

--- src/extract_clusters_with_kmeans.py
+++ src/extract_clusters_with_kmeans.py
@@ -1,6 +1,6 @@
 import logging
 
-from functools import partial
+from functools import filterfalse, partial
 
 from nltk import download as nltk_download
 from nltk.corpus import stopwords
@@ -44,7 +44,7 @@ with Timer("Normalizing tweets' text"):
     alphanumeric_corpus = map(partial(filter, lambda x: x.isalpha()), splitted_corpus)
 
     logger.info("Removing stopwords")
-    clean_corpus = list(map(partial(filter, lambda x: x in stop_words), alphanumeric_corpus))
+    clean_corpus = map(partial(filterfalse, lambda x: x not in stop_words), alphanumeric_corpus)
     final_corpus = list(map(list, clean_corpus))
 
 logger.info("Clean tweet example: %s", final_corpus[0])
