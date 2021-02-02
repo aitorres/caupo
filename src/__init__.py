70826f139c4781ec7c2d00873b1966b19b994742
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-02-01 23:51:47 -0400

    feat: configure logger for project

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+# Create logger
+logger = logging.getLogger("caupo")
+logger.setLevel(logging.DEBUG)
+
+# Create formatter
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+
+# Add console (standard output) handler to the logger
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
