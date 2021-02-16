8a37419ddff7ad649962e29c68d0bc8446fea431
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-02-15 21:57:42 -0400

    chore: add on repo command that fetches tweets periodically

---
+++ src/fetch_tweets.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python twitter/fetch_tweets_by_search_term.py store search "#Venezuela OR Maduro OR Guaidó OR #SOSVenezuela OR #AN OR #Guaido OR #Maduro OR Chavez OR chavismo OR interinato OR Capriles OR Constituyente OR GNB OR FANB OR PNB OR dictadura"
+
