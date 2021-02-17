2e07626f344ce0cfda7f16960d930d52898b7852
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-02-17 01:29:40 -0400

    chore: add topics to script that fetches tweets

--- src/fetch_tweets.sh
+++ src/fetch_tweets.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-python twitter/fetch_tweets_by_search_term.py store search "#Venezuela OR Maduro OR Guaidó OR #SOSVenezuela OR #AN OR #Guaido OR #Maduro OR Chavez OR chavismo OR interinato OR Capriles OR Constituyente OR GNB OR FANB OR PNB OR dictadura"
-
+python twitter/fetch_tweets_by_search_term.py store search "#Venezuela OR Maduro OR Guaidó OR #SOSVenezuela OR #AN OR #Guaido OR #Maduro OR Chavez OR chavismo OR interinato OR Capriles OR Constituyente OR GNB OR FANB OR PNB OR dictadura OR PrimeroJusticia OR VenteVenezuela OR VoluntadPopular OR AD OR embajada OR presidente OR vicepresidente OR vicepresidenta OR ministro OR ministerio OR ministros"
