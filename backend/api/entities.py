21fd89557ea555d1081d5192ba34b863c93ec6ee
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-04-14 03:54:14 -0400

    feat: add new endpoint that returns a b64 encoded wordcloud for a given entity frequency

--- backend/api/entities.py
+++ backend/api/entities.py
@@ -5,11 +5,14 @@ from the dataset of tweets periodically.
 Connected to the main Flask application through a Blueprint.
 """
 
+import base64
 import logging
+from io import BytesIO
 from typing import Any, Dict, List, Tuple
 
 import pymongo
 from flask import Blueprint
+from wordcloud import WordCloud
 
 # Initializing logger
 logger = logging.getLogger()
@@ -30,6 +33,30 @@ VALID_FREQUENCIES = [
 
 
 # Endpoints
+@blueprint.route('/wordcloud/get/<frequency>', methods=['GET'])
+def get_wordcloud(frequency: str) -> Tuple[Dict[str, Any], int]:
+    """
+    Given a frequency, returns the base64-encoded data of an image that represents the
+    wordcloud of all entities found in the latest tag of said frequency
+    """
+
+    if frequency not in VALID_FREQUENCIES:
+        logger.error("[get_entities] Tried to get entities with invalid frequency `%s`", frequency)
+        return {
+            'httpStatus': 400,
+            'message': f'You requested an invalid or unrecognized frequency (`{frequency}`)'
+        }, 400
+
+    # Getting latest entity tag
+    entity = _fetch_entities(frequency, 1)[0]
+    image_data = _get_b64_wordcloud(entity)
+
+    return {
+        'httpStatus': 200,
+        'message': 'Wordcloud generated collected successfully',
+        'data': image_data
+    }, 200
+
 @blueprint.route('/get/<frequency>', methods=['GET'])
 @blueprint.route('/get/<frequency>/<amount>', methods=['GET'])
 def get_entities(frequency: str, amount: int = 0) -> Tuple[Dict[str, Any], int]:
@@ -88,6 +115,20 @@ def _fetch_entities(frequency: str, amount: int = 0) -> List[Dict[str, Any]]:
     if amount > 0:
         entities = entities.limit(amount)
     return list(entities)
+
+
+def _get_b64_wordcloud(entity) -> str:
+    """
+    Given an entity tag, generates a wordcloud from all of its entities and returns
+    a base64 encoded string representing said wordcloud
+    """
+
+    wc = WordCloud().generate(" ".join(entity['entities']['all']['list']))
+    buffer = BytesIO()
+    wc.to_image().save(buffer, 'png')
+    b64 = base64.b64encode(buffer.getvalue())
+
+    return b64
 
 
 # TODO: Unify with similar function in main `src` of project
