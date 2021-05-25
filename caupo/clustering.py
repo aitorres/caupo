b866c260604d46b878215d18f21c34e18a5a2d80
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-05-24 22:23:00 -0400

    feat: add more algorithms and improve testing client

--- caupo/clustering.py
+++ caupo/clustering.py
@@ -8,7 +8,8 @@ from typing import Dict, List, Optional
 
 import numpy as np
 from hdbscan import HDBSCAN
-from sklearn.cluster import OPTICS, AffinityPropagation, KMeans
+from sklearn.cluster import (OPTICS, AffinityPropagation, KMeans, MeanShift,
+                             SpectralClustering)
 from sklearn.metrics import silhouette_score
 
 logger = logging.getLogger("caupo")
@@ -128,7 +129,7 @@ class AffinityPropagationClustering(BaseClustering):
     on sklearn.
     """
 
-    def __init__(self, min_samples: int = 10):
+    def __init__(self):
         """Instantiates a new instance of the Affinity Propagation Clustering wrapper class"""
 
         logger.debug("Initializing AffinityPropagationClustering")
@@ -141,19 +142,93 @@ class AffinityPropagationClustering(BaseClustering):
         return self.model.labels_
 
 
+class MeanShiftClustering(BaseClustering):
+    """
+    Mean Shift Clustering wrapper class. Performs clustering using optics's algorithm as implemented
+    on sklearn.
+    """
+
+    def __init__(self):
+        """Instantiates a new instance of the Mean Shift Clustering wrapper class"""
+
+        logger.debug("Initializing MeanShiftClustering")
+        self.model = MeanShift(n_jobs=-1)
+
+    def cluster(self, vectors: List[List[float]]) -> List[float]:
+        """Given a list of vectors, performs hdbscan based clustering and returns the output labels"""
+
+        self.model.fit_predict(vectors)
+        return self.model.labels_
+
+
+class SpectClustering(BaseClustering):
+    """
+    Mean Shift Clustering wrapper class. Performs clustering using optics's algorithm as implemented
+    on sklearn.  If not specified, automatically detects the proper value of
+    K that fits the algorithm best.
+    """
+
+    MIN_K = 2
+    MAX_K = 5
+
+    def __init__(self, k: Optional[int] = None) -> None:
+        """Initializes a new instance of the KMeans clustering algorithm"""
+
+        if k is not None:
+            if not self.MIN_K <= k <= self.MAX_K:
+                raise ValueError(k)
+
+            self.k = k
+            self.model = self.instantiate_model(self.k, n_jobs=-1)
+            logger.debug("Initializing SpectClustering with k=`%s`", self.k)
+        else:
+            self.k = None
+            logger.debug("Deferring Spectral Clustering model initialization until vectors are known")
+
+    def instantiate_model(self, k: int) -> SpectralClustering:
+        """Instantiates Sklearn's KMeans class and stores within wrapper class"""
+
+        return SpectralClustering(n_clusters=k, n_jobs=-1)
+
+    def cluster(self, vectors: List[List[float]]) -> List[float]:
+        """Given a list of vectors, performs kmeans based clustering and returns labels of the output"""
+
+        # Easy case: we know `k` beforehand
+        if self.k is not None:
+            return self.model.fit_predict(vectors)
+
+        # Determining best K
+        sil_scores = {}
+        for k in range(self.MIN_K, self.MAX_K + 1):
+            model = self.instantiate_model(k)
+            output = model.fit_predict(vectors)
+            sil_score = silhouette_score(vectors, output)
+            sil_scores[k] = sil_score
+
+        # Settings best K
+        max_sil = sorted(sil_scores.items(), key=lambda x: x[1], reverse=True)[0]
+        self.k = max_sil[0]
+        self.model = self.instantiate_model(self.k)
+        logger.debug("Maximum silhouette score achieved with k=%s (silhouette score: %s)", max_sil[0], max_sil[1])
+
+        # Recursive call will get the real result
+        return self.cluster(vectors)
+
+
 def get_clustering_functions() -> Dict[str, BaseClustering]:
     """
     Returns a dictionary of the available clustering algorithms,
     indexable by their names.
     """
 
+    # TODO: Consider including DBSCAN
     return {
         'k-means': KMeansClustering(),
         'hdbscan': HdbscanClustering(),
         'affinity': AffinityPropagationClustering(),
-        # TODO: dbscan
-        # TODO: meanshift
+        'mean-shift': MeanShiftClustering(),
         'optics': OpticsClustering(),
+        'spectral': SpectClustering(),
     }
 
 
@@ -162,10 +237,21 @@ def main() -> None:
 
     vectors = [np.random.random(3) for _ in range(150)]
 
+    results = {}
     for name, algorithm in get_clustering_functions().items():
         output = algorithm.cluster(vectors)
-        logger.info(f"{name} result: {output}")
-        logger.info(f"{name} sil score: {silhouette_score(vectors, output)}")
+        logger.debug(f"{name} result: {output}")
+        try:
+            sil_score = silhouette_score(vectors, output)
+            logger.debug(f"{name} sil score: {sil_score}")
+            results[name] = sil_score
+        except ValueError:
+            logger.error(f"couldn't get sil score for {name}")
+
+    sorted_results = sorted(results.items(), key=lambda x: x[1], reverse=True)
+    logger.debug("silhouette score rsults")
+    for name, sil_score in sorted_results:
+        logger.debug(f"{name}: {sil_score}")
 
 
 if __name__ == "__main__":
