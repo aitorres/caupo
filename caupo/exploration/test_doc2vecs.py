853747b6c5a170293f912721609b2a2c5f990e31
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-06-17 20:59:26 -0400

    fix: reorder

--- caupo/exploration/test_doc2vecs.py
+++ caupo/exploration/test_doc2vecs.py
@@ -12,7 +12,7 @@ from caupo.tags import get_tags_by_frequency, fetch_tag_from_db
 from caupo.utils import get_main_corpus, plot_clusters
 
 
-def doc2vec_embedder(corpus: List[str], scale: bool = True, size: int = 100, window: int = 5) -> List[float]:
+def doc2vec_embedder(corpus: List[str], size: int = 100, window: int = 5) -> List[float]:
     """
     Given a corpus of texts, returns an embedding (representation
     of such texts) using a fine-tuned Doc2Vec embedder.
@@ -20,13 +20,13 @@ def doc2vec_embedder(corpus: List[str], scale: bool = True, size: int = 100, win
     ref: https://radimrehurek.com/gensim/models/doc2vec.html
     """
 
-    print(f"Training Doc2Vec with: scale={scale}, size={size}, window={window}")
+    print(f"Training Doc2Vec with: size={size}, window={window}")
     tagged_documents = [TaggedDocument(doc.split(), [i]) for i, doc in enumerate(corpus)]
     model = Doc2Vec(tagged_documents, vector_size=size, window=window, min_count=3, workers=16)
 
     def embedder(documents: List[str]) -> List[float]:
         """Generates an embedding using a Doc2Vec"""
-        return scale_vectors([model.infer_vector(doc.split()) for doc in documents], scale=scale)
+        return scale_vectors([model.infer_vector(doc.split()) for doc in documents])
 
     return embedder
 
@@ -35,8 +35,7 @@ def main() -> None:
     """Run script for tests"""
 
     windows = [3, 5, 7]
-    sizes = [50, 100, 150, 200, 400]
-    scales = [True, False]
+    sizes = [50, 100, 150]
     output_path = "outputs/exploration/doc2vecs"
     frequency = "daily"
     os.makedirs(output_path, exist_ok=True)
@@ -46,7 +45,7 @@ def main() -> None:
     tag_name, _ = get_tags_by_frequency(frequency)[0]
     print(f"Getting tweets of {tag_name}")
 
-    for stem in scales:
+    for stem in [False, True]:
         print(f"Using {stem} for stemming")
 
         print("Cleaning main corpus")
@@ -60,14 +59,13 @@ def main() -> None:
 
         for window in windows:
             for size in sizes:
-                for scale in scales:
-                    print(f"Now trying scale={scale}, size={size}, window={window}, stem={stem}")
-                    model = doc2vec_embedder(cleaned_corpus, scale, size, window)
-                    vectors = model(cleaned_tweets)
+                print(f"Now trying size={size}, window={window}, stem={stem}")
+                model = doc2vec_embedder(cleaned_corpus, size, window)
+                vectors = model(cleaned_tweets)
 
-                    print("Plotting...")
-                    plot_clusters(vectors, f"{output_path}/window{window}-size{size}-scale{scale}_stem{stem}.png",
-                                  f"scale={scale}, size={size}, window={window}, stem={stem}")
+                print("Plotting...")
+                plot_clusters(vectors, f"{output_path}/window{window}-size{size}-stem{stem}.png",
+                                f"size={size}, window={window}, stem={stem}")
     print("Done!")
 
 
