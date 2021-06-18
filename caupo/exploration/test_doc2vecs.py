b89b995ff26743c8f81c5645a7ee6acb749294a8
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-06-17 21:00:55 -0400

    feat: logs

--- caupo/exploration/test_doc2vecs.py
+++ caupo/exploration/test_doc2vecs.py
@@ -2,6 +2,7 @@
 Test script for comparing or contrasting different runs of Doc2Vec
 """
 
+import logging
 import os
 from typing import List
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
@@ -10,6 +11,8 @@ from caupo.embeddings import scale_vectors
 from caupo.preprocessing import preprocess_v2
 from caupo.tags import get_tags_by_frequency, fetch_tag_from_db
 from caupo.utils import get_main_corpus, plot_clusters
+
+logger = logging.getLogger("caupo")
 
 
 def doc2vec_embedder(corpus: List[str], size: int = 100, window: int = 5) -> List[float]:
@@ -20,7 +23,7 @@ def doc2vec_embedder(corpus: List[str], size: int = 100, window: int = 5) -> Lis
     ref: https://radimrehurek.com/gensim/models/doc2vec.html
     """
 
-    print(f"Training Doc2Vec with: size={size}, window={window}")
+    logger.info(f"Training Doc2Vec with: size={size}, window={window}")
     tagged_documents = [TaggedDocument(doc.split(), [i]) for i, doc in enumerate(corpus)]
     model = Doc2Vec(tagged_documents, vector_size=size, window=window, min_count=3, workers=16)
 
@@ -40,33 +43,33 @@ def main() -> None:
     frequency = "daily"
     os.makedirs(output_path, exist_ok=True)
 
-    print("Getting main corpus")
+    logger.info("Getting main corpus")
     corpus = get_main_corpus()
     tag_name, _ = get_tags_by_frequency(frequency)[0]
-    print(f"Getting tweets of {tag_name}")
+    logger.info(f"Getting tweets of {tag_name}")
 
     for stem in [False, True]:
-        print(f"Using {stem} for stemming")
+        logger.info(f"Using {stem} for stemming")
 
-        print("Cleaning main corpus")
+        logger.info("Cleaning main corpus")
         cleaned_corpus = list(set(map(lambda t: preprocess_v2(t, stem), corpus)))
 
         tag = fetch_tag_from_db(frequency, tag_name)
         tweets = tag["tweets"]
 
-        print("Cleaning tweets")
+        logger.info("Cleaning tweets")
         cleaned_tweets = list(set(map(lambda t: preprocess_v2(t, stem), tweets)))
 
         for window in windows:
             for size in sizes:
-                print(f"Now trying size={size}, window={window}, stem={stem}")
+                logger.info(f"Now trying size={size}, window={window}, stem={stem}")
                 model = doc2vec_embedder(cleaned_corpus, size, window)
                 vectors = model(cleaned_tweets)
 
-                print("Plotting...")
+                logger.info("Plotting...")
                 plot_clusters(vectors, f"{output_path}/window{window}-size{size}-stem{stem}.png",
-                                f"size={size}, window={window}, stem={stem}")
-    print("Done!")
+                              f"size={size}, window={window}, stem={stem}")
+    logger.info("Done!")
 
 
 if __name__ == "__main__":
